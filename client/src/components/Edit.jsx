fbaf71ff651457e1b381f7069125bf7895da5dbc
Gabriel Kuzava <101483110+kuzag015@users.noreply.github.com>
2022-09-22 16:00:13 -0700

    Added labels for Edit.jsx component

--- client/src/components/Edit.jsx
+++ client/src/components/Edit.jsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react'
 import axios from "axios"
 import {useParams, useNavigate} from "react-router-dom"
 
+import ("./Edit.css")
+
 const EditSong = (props) => {
     const { id } = useParams();
     const [title, setTitle] = useState();
@@ -54,24 +56,28 @@ const EditSong = (props) => {
                     <form onSubmit={submitHandler} className='centered'>
                     <div className="row spaced">
                         <div className='col-12'>
+                            <label className = "labelFont" htmlFor="title">Song Title</label>
                             <input onChange={(e)=>{setTitle(e.target.value)}} type="text" className="form-control" id="title" placeholder={title} value={title}/>
                             {errors.title ? <p>{errors.title.message}</p> : null} 
                         </div>
                     </div>
                     <div className="row spaced">
                         <div className='col-12'>
+                            <label className = "labelFont" htmlFor="artist">Artist</label>
                             <input onChange={(e)=>{setArtist(e.target.value)}} type="text" className="form-control" id="artist" placeholder={artist} value={artist}/>
                         </div>
                     </div>
                     {errors.artist ? <p>{errors.artist.message}</p> : null}
                     <div className="row spaced">
                         <div className='col-12'>
+                            <label className = "labelFont" htmlFor="image">Image URL</label>
                             <input onChange={(e)=>{setImage(e.target.value)}} type="text" className="form-control" id="image" placeholder={image} value={image}/>
                             {errors.image ? <p>{errors.image.message}</p> : null}
                         </div>
                     </div>
                     <div className="row spaced">
                         <div className='col-12'>
+                            <label className = "labelFont" htmlFor="title">How does this song make you feel?</label>
                             <textarea onChange={(e)=>{setComment(e.target.value)}} className="form-control" rows={8} id="comment" placeholder={comment} value={comment}/>
                             {errors.comment ? <p>{errors.comment.message}</p> : null}
                         </div>
